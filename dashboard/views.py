c7fa018e8f66bd872f6aa02b0f0e9953c9db689b
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-06 16:35:19 +0200

    [view] - add index

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from dashboard.models import Task, Worker
+
+
+def index(request):
+    num_tasks = Task.objects.count()
+    num_workers = Worker.objects.count()
+
+    context = {
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+    }
+
+    return render(request, "dashboard/index.html", context=context)
