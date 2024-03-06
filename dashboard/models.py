1781c7593fc44b7ffc2e604d55e5b4e6edb2e33b
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-06 13:52:02 +0200

    Create models: Position, Worker, Task, TaskType

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Task(models.Model):
+    CHOICES = (
+        (1, "Low"),
+        (2, "Medium"),
+        (3, "High"),
+        (4, "Urgent"),
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=20, choices=CHOICES)
