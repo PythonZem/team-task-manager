c0c25092f66a73be3bc61fe72209dffc6b330566
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-06 15:35:22 +0200

    Fix position feild in Worker model

--- dashboard/models.py
+++ dashboard/models.py
@@ -10,7 +10,7 @@ class Position(models.Model):
 
 
 class Worker(AbstractUser):
-    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.username
@@ -33,7 +33,6 @@ class Task(models.Model):
 
     name = models.CharField(max_length=255)
     description = models.TextField()
-    deadline = models.DateField()
     deadline = models.DateTimeField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
     priority = models.CharField(max_length=20, choices=CHOICES)
