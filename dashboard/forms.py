efb4c1ee6099e27edeae4b5c7be5b18e4427de1c
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-07 20:19:19 +0200

    [Forms] Add TaskForm

---
+++ dashboard/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from dashboard.models import Task, Worker, Project
+
+
+class TaskForm(forms.ModelForm):
+    assignee = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    deadline = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+
+    class Meta:
+        model = Task
+        fields = "__all__"
