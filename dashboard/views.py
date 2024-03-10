854486e3fe079247e86e6883fdc620b093c73a40
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-10 22:46:39 +0200

    Implament views func for (Tast, Project, Worker) Model,

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,7 +14,6 @@ from django.views.generic import (
 )
 
 
-from dashboard.forms import TaskForm, ProjectCreateForm, WorkerCreationForm
 from dashboard.forms import TaskForm, WorkerForm, ProjectForm, WorkerCreateForm
 from dashboard.models import Task, Worker, Project
 
@@ -41,29 +40,102 @@ def index(request):
 
 class TaskListView(ListView):
     model = Task
-    paginate_by = 20
+    paginate_by = 10
 
 
 class TaskDetailView(DetailView):
     model = Task
 
 
-class TaskCreateView(CreateView):
+class MyTaskUpdateView(UpdateView):
     model = Task
     form_class = TaskForm
-    success_url = reverse_lazy("task-list")
+    success_url = reverse_lazy("my-task-list")
 
 
-class WorkerCreateView(LoginRequiredMixin, CreateView):
-    model = Worker
-    form_class = WorkerCreationForm
-    success_url = reverse_lazy("worker-list")
+class ProjectTaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("project-detail")
+
+    def get_success_url(self):
+        project_id = Task.objects.get(id=self.kwargs["pk"]).project_id
+        return reverse_lazy("project-detail", kwargs={"pk": project_id})
+
+
+@login_required()
+def task_delete_in_project(request, pk):
+    project_id = Task.objects.get(id=pk).project_id
+    Task.objects.get(id=pk).delete()
+    return HttpResponseRedirect(
+        reverse_lazy(viewname="project-detail", args=[project_id])
+    )
+
+
+@login_required()
+def task_delete_in_my_task(request, pk):
+    Task.objects.get(id=pk).delete()
+    return HttpResponseRedirect(reverse_lazy(viewname="my-task-list"))
+
+
+@login_required()
+def my_task_make_is_done(request, pk):
+    task = Task.objects.get(id=pk)
+    task.is_completed = True
+    task.save()
+    return HttpResponseRedirect(reverse_lazy(viewname="my-task-list"))
+
+
+@login_required()
+def project_task_make_is_done(request, pk):
+    task = Task.objects.get(id=pk)
+    task.is_completed = True
+    task.save()
+    project_id = Task.objects.get(id=pk).project_id
+    return HttpResponseRedirect(
+        reverse_lazy(viewname="project-detail", args=[project_id])
+    )
+
+
+@login_required
+def toggle_assign_to_task(request, pk):
+    worker = Worker.objects.get(id=request.user.id)
+    worker.task_assignee.add(pk)
+    project_id = Task.objects.get(id=pk).project_id
+    return HttpResponseRedirect(
+        reverse_lazy(viewname="project-detail", args=[project_id])
+    )
+
+
+class ProjectTaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("my-task-list")
+
+    def get_success_url(self):
+        return reverse_lazy("project-detail", kwargs={"pk": self.kwargs["pk"]})
+
+
+class MyTaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("my-task-list")
+
+
+class MyTaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = "dashboard/my_task_list.html"
+    context_object_name = "task_list"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Task.objects.filter(assignee=self.request.user)
 
 
 class MyProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "dashboard/my_project_list.html"
-    context_object_name = "projects_list"
+    context_object_name = "my_projects_list"
 
     def get_queryset(self):
         return Project.objects.filter(team=self.request.user)
@@ -81,25 +153,43 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
 
 class ProjectCreateView(LoginRequiredMixin, CreateView):
     model = Project
-    form_class = ProjectCreateForm
+    form_class = ProjectForm
     success_url = reverse_lazy("project-list")
 
 
-class MyTaskListView(LoginRequiredMixin, ListView):
-    model = Task
-    template_name = "dashboard/my_task_list.html"
-    context_object_name = "task_list"
+class ProjectUpdateView(LoginRequiredMixin, UpdateView):
+    model = Project
+    form_class = ProjectForm
 
-    def get_queryset(self):
-        return Task.objects.filter(assignee=self.request.user)
+    def get_success_url(self):
+        return reverse_lazy("project-detail", kwargs={"pk": self.kwargs["pk"]})
+
+
+class MyProjectDeleteView(LoginRequiredMixin, DeleteView):
+    model = Project
+    success_url = reverse_lazy("my-project-list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tasks = Task.objects.filter(project_id=self.kwargs["pk"])
+        context["tasks"] = tasks
+        return context
+
+
+class ProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    context_object_name = "projects_list"
+    paginate_by = 10
 
 
 class WorkerListView(LoginRequiredMixin, ListView):
     model = Worker
+    paginate_by = 10
 
 
 class WorkerUpdateView(LoginRequiredMixin, UpdateView):
     model = Worker
+    form_class = WorkerForm
 
 
 class WorkerDeleteView(LoginRequiredMixin, DeleteView):
@@ -107,11 +197,7 @@ class WorkerDeleteView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy("worker-list")
 
 
-@login_required
-def toggle_assign_to_task(request, pk):
-    worker = Worker.objects.get(id=request.user.id)
-    worker.task_assignee.add(pk)
-    project_id = Task.objects.get(id=pk).project_id
-    return HttpResponseRedirect(
-        reverse_lazy(viewname="project-detail", args=[project_id])
-    )
+class WorkerCreateView(LoginRequiredMixin, CreateView):
+    model = Worker
+    form_class = WorkerCreateForm
+    success_url = reverse_lazy("worker-list")
